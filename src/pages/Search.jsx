e2ea07437d852fefd860195d7f5f2403610fd747
Tomás Chaves <tomcchaves@gmail.com>
2023-01-05 19:02:27 -0300

    Requisito 5.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,58 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isSearchButtonDisabled: true,
+    search: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+
+    this.setState(
+      { search: value },
+      () => {
+        this.handleLength();
+      },
+    );
+  };
+
+  handleLength = () => {
+    const { search } = this.state;
+    const minimum = 2;
+    if (search.length >= minimum) {
+      console.log('deu');
+      this.setState({
+        isSearchButtonDisabled: false,
+      });
+    }
+  };
+
   render() {
+    const { isSearchButtonDisabled } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
         Search
+        <div data-testid="page-login">
+          <form>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isSearchButtonDisabled }
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
