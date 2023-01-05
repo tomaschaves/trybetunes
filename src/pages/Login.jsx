72b4c07f73a5f2f0fe1f6a974ff5c4074b077902
Tomás Chaves <tomcchaves@gmail.com>
2023-01-05 16:16:47 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,81 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    isEnterButtonDisabled: true,
+    username: '',
+    loading: false,
+  };
+
+  handleLength = () => {
+    const { username } = this.state;
+    const minimum = 3;
+    if (username.length >= minimum) {
+      this.setState({
+        isEnterButtonDisabled: false,
+      });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+
+    this.setState(
+      { username: value },
+      () => {
+        this.handleLength();
+      },
+    );
+  };
+
+  handleClick = async (event) => {
+    event.preventDefault();
+    const { username } = this.state;
+    const { history } = this.props;
+
+    this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name: username });
+      history.push('/search');
+    });
+  };
+
   render() {
+    const { isEnterButtonDisabled, loading } = this.state;
+
+    if (loading) return <h3>Carregando...</h3>;
+
     return (
       <div data-testid="page-login">
-        Login
+        <form onSubmit={ this.onSaveButtonClick }>
+          <input
+            type="text"
+            name="user"
+            id="user"
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isEnterButtonDisabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
