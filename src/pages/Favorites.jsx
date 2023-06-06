83913c256812a66d44e19b17c9e79b252389c6f1
Tomás Chaves <tomcchaves@gmail.com>
2023-06-06 18:17:56 -0300

    Requisito 13.

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -29,7 +29,7 @@ class Favorites extends Component {
     });
     this.setState({
       isLoading: false,
-    })
+    });
   };
 
   render() {
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    user: '',
+  };
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    const user = await getUser();
+    this.setState({ user });
+  };
+
   render() {
+    const { user } = this.state;
+    console.log(typeof user);
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        <Link to="/profile/edit"> Editar perfil</Link>
+        <h1>Profile</h1>
+        {
+          typeof user === 'string' && <p>Carregando...</p>
+        }
+        {
+          typeof user === 'object' && (
+            <div>
+
+              <h2>
+                { user.name }
+              </h2>
+              <h3>
+                { user.email }
+              </h3>
+              <h3>
+                { user.description }
+              </h3>
+              <img src={ user.image } alt={ user.name } data-testid="profile-image" />
+            </div>
+          )
+        }
       </div>
     );
   }
